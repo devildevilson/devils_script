ab9f6801ab2873b3b0d5a670baf0edd6514f8ea7
Devil Devilson <devildevilson000@gmail.com>
2023-10-07 18:23:18 +0600

    removed redundant declarations

--- src/header.h
+++ src/header.h
@@ -38,11 +38,6 @@ namespace DEVILS_SCRIPT_OUTER_NAMESPACE {
           return ret.get<T>();
         }
       }
-      // draw теперь не существует, вместо этого нужно вычислить полный стейт скрипта
-      // для этого нужно создать аллокатор, вызвать compute и рекурсивно пройтись по всем детям стейта
-      //void draw(context* ctx) const {
-        //begin->draw(ctx);
-      //}
 
       // compute script state
       local_state* compute(context* ctx, local_state_allocator* allocator) const {
@@ -60,56 +55,12 @@ namespace DEVILS_SCRIPT_OUTER_NAMESPACE {
       const script_data* data;
     };
 
-    class number : public base<double> {
-    public:
-      number() noexcept = default;
-      number(const script_data* begin) noexcept : base(begin) {}
-      number(const number &copy) noexcept = default;
-      number(number &&move) noexcept = default;
-      number & operator=(const number &copy) noexcept = default;
-      number & operator=(number &&move) noexcept = default;
-    };
-
-    class string : public base<std::string_view> {
-    public:
-      string() noexcept = default;
-      string(const script_data* begin) noexcept : base(begin) {}
-      string(const string &copy) noexcept = default;
-      string(string &&move) noexcept = default;
-      string & operator=(const string &copy) noexcept = default;
-      string & operator=(string &&move) noexcept = default;
-
-    };
-    class condition : public base<bool> {
-    public:
-      condition() noexcept = default;
-      condition(const script_data* begin) noexcept : base(begin) {}
-      condition(const condition &copy) noexcept = default;
-      condition(condition &&move) noexcept = default;
-      condition & operator=(const condition &copy) noexcept = default;
-      condition & operator=(condition &&move) noexcept = default;
-    };
-
-    class effect : public base<void> {
-    public:
-      effect() noexcept = default;
-      effect(const script_data* begin) noexcept : base(begin) {}
-      effect(const effect &copy) noexcept = default;
-      effect(effect &&move) noexcept = default;
-      effect & operator=(const effect &copy) noexcept = default;
-      effect & operator=(effect &&move) noexcept = default;
-    };
-
+    using number = base<double>;
+    using string = base<std::string_view>;
+    using consition = base<bool>;
+    using effect = base<void>;
     template <typename T>
-    class user_data : public base<T> {
-    public:
-      user_data() noexcept = default;
-      user_data(const script_data* begin) noexcept : base<T>(begin) {}
-      user_data(const user_data &copy) noexcept = default;
-      user_data(user_data &&move) noexcept = default;
-      user_data & operator=(const user_data &copy) noexcept = default;
-      user_data & operator=(user_data &&move) noexcept = default;
-    };
+    using user_data = base<T>;
   }
 
 #ifdef DEVILS_SCRIPT_OUTER_NAMESPACE
